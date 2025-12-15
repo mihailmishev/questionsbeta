931afefdbff541c9ac7965d26b2e8d96ed384d7b
Mihail Mishev <mihailmishev23@gmail.com>
2025-12-15 23:58:34 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
- [
+const questions = [
   {
     #: "1",
     Question: "Which process determines the risks that might affect the project?",
@@ -3659,4 +3659,4 @@
     Answer: "A",
     Images: ""
   }
-]
+];
